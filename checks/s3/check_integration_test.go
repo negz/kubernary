e2cf3fa148a3a3943fd9f06720953c53f0c913fd
Nic Cope <nic.cope@offerupnow.com>
2017-03-14 14:59:52 -0700

    Use CheckConfigFromEnv to get config for IT
    To avoid duplicate code to get config from env vars.

--- checks/s3/check_integration_test.go
+++ checks/s3/check_integration_test.go
@@ -19,26 +19,26 @@ import (
 )
 
 const (
-	envEndpoint   string = "S3_ENDPOINT"
-	envRegion     string = "S3_REGION"
-	defaultRegion string = "us-east-1"
+	cfgEndpoint string = "ENDPOINT"
+
+	defaultEndpoint string = "http://localhost:10002"
 )
 
 func TestS3CheckIntegration(t *testing.T) {
-	e := os.Getenv(envEndpoint)
+	env := map[string]string{
+		cfgRegion:   defaultRegion,
+		cfgEndpoint: defaultEndpoint,
+	}
+	env = kubernary.CheckConfigFromEnv("s3_it", env)
 
 	// NewNoopClient never returns an error.
 	s, _ := statsd.NewNoopClient()
 
-	region, ok := os.LookupEnv(envRegion)
-	if !ok {
-		region = defaultRegion
-	}
 	// Path style is necessary for https://github.com/jubos/fake-s3
-	cfg := aws.NewConfig().WithEndpoint(e).WithRegion(region).WithS3ForcePathStyle(true)
+	cfg := aws.NewConfig().WithEndpoint(env[cfgEndpoint]).WithRegion(env[cfgRegion]).WithS3ForcePathStyle(true)
 	session, err := session.NewSession(cfg)
 	if err != nil {
-		t.Fatalf("Unable to create new AWS session against endpoint %v: %v", e, err)
+		t.Fatalf("Unable to create new AWS session against endpoint %v: %v", env[cfgEndpoint], err)
 	}
 	d := s3manager.NewDownloader(session)
 
@@ -53,19 +53,21 @@ func TestS3CheckIntegration(t *testing.T) {
 			t.Fatalf("New(KeyExists, %s, Downloader(%s), Logger(%s): %v", s, d, l)
 		}
 		if err := check.Check(); err != nil {
-			t.Fatalf("Want data at endpoint %s to exist, but check says it does not.", e)
+			t.Fatalf("Want data at endpoint %s to exist, but check says it does not.", env[cfgEndpoint])
 		}
 	})
 
 	t.Run("KeyProbablyDoesNotExist", func(t *testing.T) {
-		os.Setenv(fmt.Sprintf("%s_%s", kubernary.CheckConfigEnvPrefix, "KEYPROBABLYDOESNOTEXIST_BUCKET"), probablyDoesNotExist())
-		os.Setenv(fmt.Sprintf("%s_%s", kubernary.CheckConfigEnvPrefix, "KEYPROBABLYDOESNOTEXIST_KEY"), probablyDoesNotExist())
+		b := fmt.Sprintf("%s%s", kubernary.CheckConfigEnvPrefix, "KEYPROBABLYDOESNOTEXIST_BUCKET")
+		k := fmt.Sprintf("%s%s", kubernary.CheckConfigEnvPrefix, "KEYPROBABLYDOESNOTEXIST_BUCKET")
+		os.Setenv(b, probablyDoesNotExist())
+		os.Setenv(k, probablyDoesNotExist())
 		check, err := New("KeyProbablyDoesNotExist", s, Downloader(d), Logger(l))
 		if err != nil {
 			t.Fatalf("New(KeyProbablyDoesNotExist, %s, Downloader(%s), Logger(%s): %v", s, d, l)
 		}
 		if err := check.Check(); err == nil {
-			t.Fatalf("Want data at endpoint %s to be absent, but check says it exists", e)
+			t.Fatalf("Want data at endpoint %s to be absent, but check says it exists", env[cfgEndpoint])
 		}
 	})
 }
